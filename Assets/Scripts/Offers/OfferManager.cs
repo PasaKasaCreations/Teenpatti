6241b4e8366fb777a90037e587526d88d94c7f6d
Shreedesh <nshreedesh@gmail.com>
2024-12-11 22:06:26 +0545

    fetch offer data

--- Assets/Scripts/Offers/OfferManager.cs
+++ Assets/Scripts/Offers/OfferManager.cs
@@ -1,9 +1,33 @@
+using API;
+using Constants;
+using ScriptableObjects.Logging;
+using Teenpatti.Data.API;
 using UnityEngine;
 
 namespace Teenpatti.Offers
 {
     public class OfferManager : MonoBehaviour
     {
-       
+        [Header("Offer Data")]
+        [SerializeField]
+        private OfferData[] offerData;
+
+        [Header("Logger")]
+        [SerializeField]
+        private Debugger apiLogger;
+
+        [ContextMenu("Get Offers")]
+        private void GetDailyRewards()
+        {
+            APIManager.Instance.Get<OfferResponse>(APIConstants.GetOffers,
+            (response) =>
+            {
+                offerData = response.data;
+            },
+            (error) =>
+            {
+                apiLogger.Log(error.message, Enums.LoggingType.Warning);
+            });
+        }
     }
 }
