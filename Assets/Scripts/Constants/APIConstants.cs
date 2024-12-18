76b389c91a9fdc7c1902e521fec189829732ff9b
Shreedesh <nshreedesh@gmail.com>
2024-12-18 20:42:26 +0545

    add android logcat and update api constants

--- Assets/Scripts/Constants/APIConstants.cs
+++ Assets/Scripts/Constants/APIConstants.cs
@@ -3,8 +3,7 @@ namespace Constants
 {
     public static class APIConstants
     {
-        //public const string APIURI = "https://api-3.pasakasacreations.com/api/v1/";
-        public const string APIURI = "https://dwvb0ytuvdxi.share.zrok.io/api/v1/";
+        public const string APIURI = "https://api-3.pasakasacreations.com/api/v1/";
 
         public const string GuestLogin = APIURI + "auth/guest/login";
         public const string RefreshLogin = APIURI + "auth/refresh-token";
